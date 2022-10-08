ec9b21988dd6d50007911ea7cea21049fc08730d
Lucas Sierota <lucasemanuelss@hotmail.com>
2022-10-08 01:11:38 -0500

    deps: add react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
-import 'tailwindcss/tailwind.css'
-import { ThemeProvider } from 'next-themes'
-import Web3Provider from '@/components/Web3Provider'
-import '@rainbow-me/rainbowkit/styles.css';
+import "tailwindcss/tailwind.css";
+import { ThemeProvider } from "next-themes";
+import Web3Provider from "@/components/Web3Provider";
+import "@rainbow-me/rainbowkit/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 const App = ({ Component, pageProps }) => {
-	return (
-		<ThemeProvider attribute="class">
-			<Web3Provider>
-				<Component {...pageProps} />
-			</Web3Provider>
-		</ThemeProvider>
-	)
-}
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider attribute="class">
+        <Web3Provider>
+          <Component {...pageProps} />
+        </Web3Provider>
+      </ThemeProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+};
 
-export default App
+export default App;
