8f052540a1357b789db984c0bed193098669e0b2
Nate Mueller <inathan.m@gmail.com>
2022-10-08 16:37:41 -0500

    single button

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -5,12 +5,14 @@ import { IPlaylist } from "@spinamp/spinamp-sdk";
 import { useState, useEffect } from "react";
 import { useAccount } from "wagmi";
 import SetGoodyBag from "@/components/SetGoodyBag";
+import { pinFileToIPFS, pinJson } from "@/services/pinata/pinata";
 
 const CreateSpace = ({ defaultProfile }) => {
   const [playlist, setPlaylist] = useState<IPlaylist>();
   const [productData, setProductData] = useState<any>();
   const [lensPost, setLensPost] = useState<any>();
-  const [uri, setUri] = useState<any>()
+  const [goody, setGoody] = useState<any>();
+  const [uploading, setUploading] = useState<boolean>()
 
   const selectPlaylist = (playlist) => {
     setPlaylist(playlist);
@@ -25,19 +27,39 @@ const CreateSpace = ({ defaultProfile }) => {
     setLensPost(postData);
   };
 
-  const setGoodyBag = (uri) => {
-    setUri(uri);
-    console.log(uri);
+  const uploadToIPFS = async () => {
+    // pick out files
+    const music = goody.files.find((f) => f.path.endsWith(".wav") || f.path.endsWith(".mp3"));
+    const cover = goody.files.find(
+      (f) => f.path.endsWith(".png") || f.path.endsWith(".gif") || f.path.endsWith(".jpeg") || f.path.endsWith(".jpg")
+    );
+
+    // upload to ipfs
+    console.log('uploading files')
+    const _music = await pinFileToIPFS(music);
+    const _image = await pinFileToIPFS(cover);
+
+    console.log('uploading metadata')
+    const metadata: any = await pinJson({
+      name: goody.name,
+      description: goody.description,
+      image: `ipfs://${_image.IpfsHash}`,
+      animation_url: `ipfs://${_music.IpfsHash}`,
+      external_url: "https://joinclubspace.xyz",
+    });
+
+    return `ipfs://${metadata.IpfsHash}`;
   };
 
-  const submit = () => {
+  const submit = async () => {
+    setUploading(true)
+    console.log(playlist, productData, lensPost, goody);
+
     // TODO: send all api calls
-    console.log(
-      playlist,
-      productData,
-      lensPost,
-      uri
-    )
+    const goodyUri = await uploadToIPFS()
+    console.log(goodyUri)
+
+    setUploading(false)
   };
 
   return (
@@ -51,15 +73,15 @@ const CreateSpace = ({ defaultProfile }) => {
       <div>
         <CreateLensPost setPostData={setPostData} defaultProfile={defaultProfile} />
       </div>
-      {!uri ? <SetGoodyBag setGoodyBag={setGoodyBag} /> : <p>GoodyBag URI: {uri}</p>}
+      <SetGoodyBag setGoody={setGoody} />
 
       <button
-          className="flex w-36 mt-4 justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          onClick={submit}
-          disabled={!uri || !playlist || !lensPost || !productData}
-        >
-          Create Party
-        </button>
+        className="flex w-36 mt-4 justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+        onClick={submit}
+        disabled={!goody || !playlist || !lensPost || !productData || uploading}
+      >
+        {uploading ? "Loading..." : "Create Party"}
+      </button>
     </div>
   );
 };
