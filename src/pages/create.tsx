528a8a6e88b79dbaf8fe45e1935fd9352e7a0e8b
Lucas Sierota <lucasemanuelss@hotmail.com>
2022-10-08 15:46:47 -0500

    fix: query needs options

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -1,10 +1,10 @@
 import CreateLensPost from "@/components/CreateLensPost";
 import SelectPlaylist from "@/components/SelectPlaylist";
-import SetDecentProduct from '@/components/SetDecentProduct';
+import SetDecentProduct from "@/components/SetDecentProduct";
 import { IPlaylist } from "@spinamp/spinamp-sdk";
 import { useState, useEffect } from "react";
 import { useAccount } from "wagmi";
-import { useGetProfilesOwned } from "@/services/lens/getProfile";
+import { Profile, useGetProfilesOwned } from "@/services/lens/getProfile";
 import SetGoodyBag from "@/components/SetGoodyBag";
 
 const CreateSpace = () => {
@@ -12,9 +12,9 @@ const CreateSpace = () => {
   const [playlist, setPlaylist] = useState<IPlaylist>();
   const [productData, setProductData] = useState<any>();
   const [lensPost, setLensPost] = useState<any>();
-  const [defaultProfile, setDefaultProfile] = useState();
-  const [uri, setUri] = useState<any>()
-  const { isLoading: loadingProfiles, data: profiles } = useGetProfilesOwned(address);
+  const [defaultProfile, setDefaultProfile] = useState<Profile>();
+  const [uri, setUri] = useState<any>();
+  const { isLoading: loadingProfiles, data: profiles } = useGetProfilesOwned({}, address);
 
   useEffect(() => {
     if (profiles?.length) {
@@ -33,13 +33,13 @@ const CreateSpace = () => {
   };
 
   const setPostData = (postData) => {
-    setLensPost(postData)
-  }
+    setLensPost(postData);
+  };
 
   const setGoodyBag = (uri) => {
-    setUri(uri)
-    console.log(uri)
-  }
+    setUri(uri);
+    console.log(uri);
+  };
 
   const submit = () => {
     // TODO: send all api calls
@@ -49,12 +49,20 @@ const CreateSpace = () => {
     <div className="">
       <p>Create a Space</p>
       {!playlist ? <SelectPlaylist selectPlaylist={selectPlaylist} /> : <p>Playlist: {playlist.title}</p>}
-      {!productData ? <SetDecentProduct setDecentProduct={setDecentProduct} /> : <p>NFT FOUND! RENDER AN NFT COMPONENT</p>}
-      {!lensPost ? <CreateLensPost setPostData={setPostData} defaultProfile={defaultProfile}/> : <div>
-        <p>Lens Post:</p>
-        <p>{lensPost}</p>
-      </div> }
-      {!uri ? <SetGoodyBag setGoodyBag={setGoodyBag}/> : <p>GoodyBag URI: {uri}</p>}
+      {!productData ? (
+        <SetDecentProduct setDecentProduct={setDecentProduct} />
+      ) : (
+        <p>NFT FOUND! RENDER AN NFT COMPONENT</p>
+      )}
+      {!lensPost ? (
+        <CreateLensPost setPostData={setPostData} defaultProfile={defaultProfile} />
+      ) : (
+        <div>
+          <p>Lens Post:</p>
+          <p>{lensPost}</p>
+        </div>
+      )}
+      {!uri ? <SetGoodyBag setGoodyBag={setGoodyBag} /> : <p>GoodyBag URI: {uri}</p>}
     </div>
   );
 };
