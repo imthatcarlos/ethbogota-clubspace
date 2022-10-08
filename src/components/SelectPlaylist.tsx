c62357b901c5fdde9f8f4b0b9d2208eb6c51cf9e
Lucas Sierota <lucasemanuelss@hotmail.com>
2022-10-08 05:08:34 -0500

    fix: some types

--- src/components/SelectPlaylist.tsx
+++ src/components/SelectPlaylist.tsx
@@ -3,11 +3,12 @@ import { fetchCollectorPlaylists } from "@spinamp/spinamp-sdk";
 import { useAccount } from "wagmi";
 
 const SelectPlaylist = ({ selectPlaylist }) => {
-  const { address, status } = useAccount();
+  const { address } = useAccount();
   const [playlists, setPlaylists] = useState([]);
 
   useEffect(() => {
     fetchPlaylists();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const fetchPlaylists = async () => {
@@ -17,13 +18,13 @@ const SelectPlaylist = ({ selectPlaylist }) => {
 
   return (
     <div>
-        <p>Select a Playlist</p>
+      <p>Select a Playlist</p>
       {playlists.map(({ title, id }) => (
         <div key={id}>
           <p>{title}</p>
           <button
             className="flex w-36 justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-            onClick={() => selectPlaylist({id, title})}
+            onClick={() => selectPlaylist({ id, title })}
           >
             Select
           </button>
--- src/components/SetDecentProduct.tsx
+++ src/components/SetDecentProduct.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from "react";
 import { useAccount, useNetwork, useSigner } from "wagmi";
-import { utils } from 'ethers';
+import { utils } from "ethers";
 import { getContractData } from "@/lib/utils/decent";
 
 const SetDecentProduct = ({ setDecentProduct }) => {
   const { chain } = useNetwork();
   const { data: signer } = useSigner();
-  const [contractAddress, setContractAddress] = useState();
 
   const onContractAddressChanged = (event) => {
     const val = event.target.value;
@@ -23,13 +22,15 @@ const SetDecentProduct = ({ setDecentProduct }) => {
   return (
     <div>
       <form>
-        <div class="grid gap-6 mb-6 md:grid-cols-2">
+        <div className="grid gap-6 mb-6 md:grid-cols-2">
           <div>
-            <label for="contract_address" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Decent NFT Address</label>
+            <label htmlFor="contract_address" className="block text-sm">
+              Your Decent NFT Address
+            </label>
             <input
               type="text"
               id="contract_address"
-              class="bg-gray-50 border"
+              className="block w-fullI rounded-md dark:bg-gray-800 dark:text-white dark:border-gray-600 border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               placeholder="0x"
               required
               onChange={onContractAddressChanged}
