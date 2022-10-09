a640793cf5005b524181e6f721cdcfa474bfa1e6
Nate Mueller <inathan.m@gmail.com>
2022-10-08 23:05:01 -0500

    lens post, fix semaphore

--- src/pages/api/semaphore/join-group.ts
+++ src/pages/api/semaphore/join-group.ts
@@ -1,8 +1,15 @@
-import { contract } from "@/lib/semaphore/semaphore";
 import { utils } from "ethers";
 import { NextApiRequest, NextApiResponse } from "next";
-import redisClient from '@/lib/utils/redisClient';
+import redisClient from "@/lib/utils/redisClient";
 import { REDIS_SPACE_EXP } from "@/lib/consts";
+
+import { VERIFIER_ADDRESS } from "@/lib/consts";
+import { Contract, providers, Wallet } from "ethers";
+import contractAbi from "../../../lib/semaphore/abi.json";
+
+const provider = new providers.JsonRpcProvider(process.env.MUMBAI_URL);
+const signer = new Wallet(process.env.ADMIN_KEY, provider);
+const contract = new Contract(VERIFIER_ADDRESS, contractAbi, signer);
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const { identityCommitment, username, groupId } = req.body;
@@ -18,14 +25,9 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
     // [HACK] log that they have joined - ephemeral
     try {
-      console.log('setting redis');
-      await redisClient.set(
-        username,
-        'true',
-        'EX',
-        REDIS_SPACE_EXP
-      );
-      console.log('set!');
+      console.log("setting redis");
+      await redisClient.set(username, "true", "EX", REDIS_SPACE_EXP);
+      console.log("set!");
     } catch (error) {
       console.log(error.stack);
     }
