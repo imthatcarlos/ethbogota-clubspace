b74fdd6c4e496b172b2878f95a5d7509301a4f54
Nate Mueller <inathan.m@gmail.com>
2022-11-28 17:05:46 -0500

    theme always dark

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import { FC, useEffect, useState } from "react";
 // import ThemeSwitcher from "@/components/ThemeSwitcher";
 import { Hero } from "@/components/Hero";
+import { useTheme } from "next-themes";
 
 const Home: FC = () => {
   const [mounted, setMounted] = useState(false);
 
+  const { setTheme } = useTheme();
+
   useEffect(() => {
+    setTheme("dark");
     setMounted(true);
   }, []);
 
