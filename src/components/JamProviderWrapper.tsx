61afb527d4989aa3d1b3b6506f698292466bc6d4
Carlos Beltran <imthatcarlos@gmail.com>
2023-01-02 20:11:40 -0500

    fix: sfu config

--- src/components/JamProviderWrapper.tsx
+++ src/components/JamProviderWrapper.tsx
@@ -1,15 +1,15 @@
 import { JamProvider } from '@/lib/jam-core-react';
 import { SPACE_API_URL } from "@/lib/consts";
 
-const jamHost = process.env.JAM_HOST || 'localhost:3000';
+const jamHost = process.env.JAM_HOST || 'localhost:3001';
 const jamSchema = process.env.JAM_SCHEMA || 'https://';
 
 const JamProviderWrapper = ({ children }) => {
   const jamConfig = {
     domain: SPACE_API_URL,
     urls: {
-      jam: process.env.JAM_URL,
-      pantry: process.env.JAM_URL, // @TODO:
+      pantry: SPACE_API_URL,
+      jam: process.env.JAM_URL || `${jamSchema}${jamHost}`,
       // stun: process.env.JAM_STUN_SERVER || `stun:stun.${jamHost}:3478`,
       // turn: process.env.JAM_TURN_SERVER || `turn:turn.${jamHost}:3478`,
       // turnCredentials: {
@@ -17,8 +17,8 @@ const JamProviderWrapper = ({ children }) => {
       //   credential: process.env.JAM_TURN_SERVER_CREDENTIAL || 'yieChoi0PeoKo8ni',
       // },
     },
-    development: false, // DISABLE IN PROD
-    sfu: process.env.JAM_SFU === 'true',
+    development: false,
+    sfu: true,
   };
 
   return (
