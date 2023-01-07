72e3b6045e49291d86ee4172c0305b6f4acd15c8
Nate Mueller <inathan.m@gmail.com>
2023-01-07 15:50:43 -0500

    profile page

--- src/components/DirectToClaims.tsx
+++ src/components/DirectToClaims.tsx
@@ -4,7 +4,7 @@ const DirectToClaims = ({ address }) => (
   <div className="">
     This stream is unavailable, has already ended, or is at capacity. Try again later?
     Head to the claims page to pick up your party favor if you attended!
-    <Link href={`/claim/${address}`}>
+    <Link href={`/u/${address}`}>
       <button className="mt-8 !w-full btn max-w-xs">Party Favors 🥳</button>
     </Link>
   </div>
