98a966aaf558a7495e99c76beda30e441e6d8262
Nate Mueller <inathan.m@gmail.com>
2023-01-13 15:51:15 -0500

    push protocol notifications endpoint updated to prod channel

--- src/services/push/push.ts
+++ src/services/push/push.ts
@@ -6,23 +6,23 @@ const PK = process.env.PUSH_KEY;
 const Pkey = `0x${PK}`;
 const signer = new ethers.Wallet(Pkey);
 
-export const sendNotification = async (link) => {
+export const sendNotification = async (link: string, title?: string, body?: string) => {
   // apiResponse?.status === 204, if sent successfully!
-  const apiResponse = await PushAPI.payloads.sendNotification({
+  await PushAPI.payloads.sendNotification({
     signer,
     type: 1, // broadcast
     identityType: 2, // direct payload
     notification: {
-      title: `[SDK-TEST] Club Space:`,
-      body: `[sdk-test] Starting now!`,
+      title: `Club Space:`,
+      body: `Starting now!`,
     },
     payload: {
-      title: `[sdk-test] Club Space is starting`,
-      body: `Head over now!`,
+      title: title ?? `Club Space is starting`,
+      body: body ?? `Head over now!`,
       cta: link,
       img: "",
     },
-    channel: "eip155:5:0xD1CCfb9Fbd9A8DE0cf9950eFbCFb29adcDA81C93", // your channel address
-    env: "staging",
+    channel: "eip155:1:0xD1CCfb9Fbd9A8DE0cf9950eFbCFb29adcDA81C93", // your channel address
+    env: "prod",
   });
 };
