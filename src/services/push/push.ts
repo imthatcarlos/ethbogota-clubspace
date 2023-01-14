736b83a30770c694664ab9fc5e11426719014f86
Nate Mueller <inathan.m@gmail.com>
2023-01-14 12:56:25 -0500

    push notifications

--- src/services/push/push.ts
+++ src/services/push/push.ts
@@ -7,22 +7,23 @@ const Pkey = `0x${PK}`;
 const signer = new ethers.Wallet(Pkey);
 
 export const sendNotification = async (link: string, title?: string, body?: string) => {
-  // apiResponse?.status === 204, if sent successfully!
+  const handle = link.slice(link.lastIndexOf("/") + 1);
+  const prod = process.env.NEXT_PUBLIC_IS_PRODUCTION === "true";
   await PushAPI.payloads.sendNotification({
     signer,
     type: 1, // broadcast
     identityType: 2, // direct payload
     notification: {
-      title: `Club Space:`,
-      body: `Starting now!`,
+      title: `Club Space by ${handle} is starting`,
+      body: `Join now!`,
     },
     payload: {
-      title: title ?? `Club Space is starting`,
-      body: body ?? `Head over now!`,
+      title: title ?? `Club Space by ${handle} is starting`,
+      body: body ?? `Join now!`,
       cta: link,
       img: "",
     },
-    channel: "eip155:1:0xD1CCfb9Fbd9A8DE0cf9950eFbCFb29adcDA81C93", // your channel address
-    env: "prod",
+    channel: `eip155:${prod ? 1 : 5}:0xD1CCfb9Fbd9A8DE0cf9950eFbCFb29adcDA81C93`,
+    env: prod ? "prod" : "staging",
   });
 };
