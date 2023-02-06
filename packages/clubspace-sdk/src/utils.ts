f588f85be65de49fc45ccf9703d573d114208fd7
Nate Mueller <inathan.m@gmail.com>
2023-02-05 22:06:43 -0500

    update utils

--- packages/clubspace-sdk/src/utils.ts
+++ packages/clubspace-sdk/src/utils.ts
@@ -1,7 +1,13 @@
+import { BigNumber } from 'ethers';
+
+const APP_NAME = 'Clubspace';
+
 export const fieldNamePrivy = (semGroupIdHex: any) => {
-    semGroupIdHex = semGroupIdHex.toString();
-    if (!semGroupIdHex.startsWith("0x")) {
-      semGroupIdHex = `0x${semGroupIdHex}`;
-    }
-    return `CLUBSPACE-${parseInt(semGroupIdHex.toString(), 16).toString()}`;
-  };
+  semGroupIdHex = semGroupIdHex.toString().replace(/-/g, '');
+  if (!semGroupIdHex.startsWith('0x')) {
+    // semaphore limitation
+    semGroupIdHex = `0x${semGroupIdHex}`;
+  }
+  const id = BigNumber.from(semGroupIdHex).toString();
+  return `${APP_NAME.toLowerCase()}-${id}`;
+};
