c33c51440cfb7c3a19a0a753bf789c76f36220ce
Lucas Sierota <lucasemanuelss@hotmail.com>
2023-03-27 22:21:14 -0300

    feat: embed player
    - add better icons
    - add transitions to volume slider
    - add shimmer loading to image
    - improve overall UI

--- src/pages/embed/[handle].tsx
+++ src/pages/embed/[handle].tsx
@@ -3,10 +3,11 @@ import { SpaceEnded } from "@/components/SpaceEnded";
 import { LENSTER_URL, NEXT_PUBLIC_SITE_URL } from "@/lib/consts";
 import { useGetProfilesByHandles } from "@/services/lens/getProfile";
 import { GetServerSideProps } from "next";
+import { Transition } from "@headlessui/react";
+import ClubspaceNeonHeader from "@/assets/svg/clubspace-neon-header.svg";
 
-import { useRouter } from "next/router";
 import type { IClubSpaceObject, ITrack } from "packages/clubspace-sdk/src";
-import { useEffect, useMemo, useRef, useState } from "react";
+import { SVGProps, useEffect, useMemo, useRef, useState } from "react";
 import { NextPageWithLayout } from "../_app";
 
 const AUDIO_PLAYER_DEFAULT_PLAYBACK = "html5";
@@ -21,8 +22,10 @@ const EmbedSpace: NextPageWithLayout = ({
   handle: string;
 }) => {
   const [hasSpaceEnded, setHasSpaceEnded] = useState(false);
+  const [showVolume, setShowVolume] = useState(false);
   const [currentTrack, setCurrentTrack] = useState<ITrack>();
   const [loading, setLoading] = useState(true);
+  const [imageLoaded, setImageLoaded] = useState(false);
   const [playing, setPlaying] = useState(false);
   const audioRef = useRef<any | null>(null);
 
@@ -62,7 +65,7 @@ const EmbedSpace: NextPageWithLayout = ({
       const fetchData = async () => {
         // force imports on client
         const { getAudioPlayer } = await import("packages/clubspace-sdk/src/audioPlayer");
-        
+
         audioRef.current = await getAudioPlayer(clubSpaceObject, playlistTracks, {
           playbackMethod: AUDIO_PLAYER_DEFAULT_PLAYBACK,
           onTrackChanged: (track) => setCurrentTrack(track),
@@ -106,147 +109,104 @@ const EmbedSpace: NextPageWithLayout = ({
     );
 
   return (
-    <div className="relative w-full h-screen">
-      {/* <NextSeo
-          title={`ClubSpace | ${clubSpaceObject.creatorLensHandle}`}
-          description={`Join @${clubSpaceObject.creatorLensHandle} at their live listening party now!`}
-          openGraph={{
-            url: `${NEXT_PUBLIC_SITE_URL}/live/${clubSpaceObject.creatorLensHandle}`,
-            title: `ClubSpace | ${clubSpaceObject.creatorLensHandle}`,
-            description: `Join @${clubSpaceObject.creatorLensHandle} at their live listening party now!`,
-            images: [
-              {
-                url: "https://link.storjshare.io/raw/jwg3vujynjlvbn5gdgm5yjoob7mq/misc%2Fclubspace.png",
-                width: 1200,
-                height: 630,
-                type: "image/png",
-                alt: "clubspace.png",
-              },
-            ],
-          }}
-        /> */}
-      {/* <pre>
-          <code>{JSON.stringify(currentTrack, null, 2)}</code>
-        </pre> */}
-
-      {/* <pre>
-          <code>{JSON.stringify(clubSpaceObject, null, 2)}</code>
-        </pre> */}
-
+    <div className="relative w-full h-[100dvh]">
       {!hasSpaceEnded && (
-        <div className="w-full h-full flex flex-col gap-2 p-8">
-          <div className="w-full h-full flex items-center justify-center gap-4">
-            <div className="w-full h-full flex flex-col gap-4">
-              <div className="relative xs:max-h-[calc(100%-60px)] flex items-center justify-center">
+        <>
+          {/* test adding the ref and updating the sdk if doesn't work */}
+          <audio className="hidden invisible" id="backup-audio"></audio>
+          <div className="w-full h-full max-h-full flex flex-col gap-2 py-8">
+            <div className="w-full h-[90%] max-h-full flex gap-4 px-8">
+              {/* image */}
+              <div className="w-full min-w-[50%] h-auto">
+                {/* shimmer effect when image is loading */}
+                {!imageLoaded && (
+                  <div
+                    className={`h-full max-w-[85%] rounded relative before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-rose-100/10 before:to-transparent isolate overflow-hidden shadow-xl shadow-black/5 before:border-t before:border-rose-100/10`}
+                  ></div>
+                )}
                 <img
                   src={currentTrackImage || hostLensData?.[0]?.picture?.uri}
                   alt={currentTrackImage ? `${currentTrack.title} banner` : `${handle} profile pic`}
-                  className="object-contain h-full aspect-square rounded self-center"
+                  className={`object-contain h-full aspect-square rounded self-center ${!imageLoaded && "hidden"}`}
                   draggable="false"
+                  loading="eager"
+                  onLoad={() => setImageLoaded(true)}
                 />
               </div>
-              {/* test adding the ref and updating the sdk if doesn't work */}
-              <audio className="hidden invisible" id="backup-audio"></audio>
-              <div className="flex gap-2 items-center justify-center w-full">
-                <button
-                  onClick={() => {
-                    if (!audioRef.current) return;
-                    if (audioRef.current.state !== "playing") return play();
-                    return pause();
-                  }}
+              {/* name - follow */}
+              <div className="w-full flex flex-col gap-1">
+                <ClubspaceNeonHeader height={45} width={80} className="place-self-end" />
+                {currentTrack && (
+                  <p className="text-left font-semibold text-2xl">
+                    {currentTrack?.title} - {currentTrack?.artist?.name}
+                  </p>
+                )}
+                <a
+                  href={`${LENSTER_URL}/u/${handle}`}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="text-gray-400 text-sm"
                 >
-                  {playing ? (
-                    <svg viewBox="0 0 24 24" fill="none" className="w-10 h-10">
-                      <g strokeWidth="0"></g>
-                      <g strokeLinecap="round" strokeLinejoin="round"></g>
-                      <g>
-                        <path
-                          opacity="0.1"
-                          d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                          fill="#ffffff"
-                        ></path>
-                        <path
-                          d="M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z"
-                          stroke="#ffffff"
-                          strokeWidth="2"
-                        ></path>
-                        <path
-                          d="M14 9L14 15"
-                          stroke="#ffffff"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        ></path>
-                        <path
-                          d="M10 9L10 15"
-                          stroke="#ffffff"
-                          strokeWidth="2"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                        ></path>
-                      </g>
-                    </svg>
-                  ) : (
-                    <svg viewBox="0 0 24 24" fill="#ffffff" className="w-12 h-12">
-                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-                      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-                      <g id="SVGRepo_iconCarrier">
-                        <rect id="view-box" width="24" height="24" fill="none"></rect>
-                        <path
-                          id="Shape"
-                          d="M0,9.75A9.75,9.75,0,1,1,9.75,19.5,9.761,9.761,0,0,1,0,9.75Zm1.5,0A8.25,8.25,0,1,0,9.75,1.5,8.259,8.259,0,0,0,1.5,9.75Zm5.75-3.8,7,3.8-7,3.8Z"
-                          transform="translate(2.25 2.25)"
-                          fill="#ffffff"
-                        ></path>
-                      </g>
-                    </svg>
-                  )}
-                </button>
-                <Slider
-                  defaultValue={[50]}
-                  max={100}
-                  step={1}
-                  onValueChange={(volumeArr) => (audioRef.current.audioElement.volume = volumeArr[0] / 100)}
-                  aria-label="Volume"
-                  className="w-full h-5"
-                />
+                  @{handle}
+                </a>
+
+                {/* Checkout live */}
+                <span className="flex flex-col gap-1 mt-1 items-center">
+                  {clubSpaceObject.stats.activeUsersInRoomCount} people connected to the space.
+                  <a
+                    href={`${NEXT_PUBLIC_SITE_URL}/live/${handle}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="inline btn !bg-[#d1157083]"
+                  >
+                    <span className="whitespace-nowrap flex items-center gap-2 text-white">
+                      Join them <ArrowRight className="w-4 h-4 inline" />
+                    </span>
+                  </a>
+                </span>
               </div>
             </div>
-            <div className="w-full h-full flex flex-col justify-center items-center gap-4">
-              {currentTrack && (
-                <p className="text-center">
-                  {currentTrack?.title} - {currentTrack?.artist?.name}
-                </p>
-              )}
-              <a href={`${LENSTER_URL}/u/${handle}`} rel="noopener noreferrer" target="_blank">
-                Follow @{handle}
-              </a>
+            {/* Play - slider */}
+            <div className="flex gap-2 items-center justify-center w-full px-6">
+              <button
+                onClick={() => {
+                  if (!audioRef.current) return;
+                  if (audioRef.current.state !== "playing") return play();
+                  return pause();
+                }}
+              >
+                {!playing ? <PlayingIcon className="w-8 h-8" /> : <PauseIcon className="w-8 h-8" />}
+              </button>
+              <div className="flex items-center w-full">
+                <VolumeIcon
+                  className="w-12 h-12"
+                  onClick={() => {
+                    setShowVolume((oldValue) => !oldValue);
+                  }}
+                />
+                <Transition
+                  show={showVolume}
+                  enter="transition-transform duration-75"
+                  enterFrom="scale-0"
+                  enterTo="scale-1"
+                  leave="transition-transform duration-150"
+                  leaveFrom="scale-1"
+                  leaveTo="scale-0"
+                  className="w-full"
+                >
+                  <Slider
+                    defaultValue={[50]}
+                    max={100}
+                    step={1}
+                    onValueChange={(volumeArr) => (audioRef.current.audioElement.volume = volumeArr[0] / 100)}
+                    aria-label="Volume"
+                    className="w-full h-5"
+                  />
+                </Transition>
+              </div>
             </div>
           </div>
-          <span className="flex gap-1 items-center">
-            <a
-              href={`${NEXT_PUBLIC_SITE_URL}/live/${handle}`}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="inline"
-            >
-              {clubSpaceObject.stats.activeUsersInRoomCount} people connected to the space. Join them
-            </a>
-            <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-4 h-4">
-              <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
-              <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
-              <g id="SVGRepo_iconCarrier">
-                <path
-                  stroke="#ffffff"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M2 10h16m0 0l-7-7m7 7l-7 7"
-                ></path>
-              </g>
-            </svg>
-          </span>
-        </div>
+        </>
       )}
     </div>
   );
@@ -291,3 +251,78 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 };
 
 export default EmbedSpace;
+
+const PlayingIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g strokeWidth="0"></g>
+      <g strokeLinecap="round" strokeLinejoin="round"></g>
+      <g>
+        <path
+          d="M8.286 3.407A1.5 1.5 0 0 0 6 4.684v14.632a1.5 1.5 0 0 0 2.286 1.277l11.888-7.316a1.5 1.5 0 0 0 0-2.555L8.286 3.407z"
+          fill="#ffffff"
+        ></path>
+      </g>
+    </svg>
+  );
+};
+
+const PauseIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      fill="#ffffff"
+      viewBox="-5.5 0 32 32"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#ffffff"
+      {...props}
+    >
+      <g strokeWidth="0"></g>
+      <g strokeLinecap="round" strokeLinejoin="round"></g>
+      <g>
+        {" "}
+        <title>pause</title>{" "}
+        <path d="M0 6.563v18.875c0 0.531 0.438 0.969 0.969 0.969h6.625c0.5 0 0.906-0.438 0.906-0.969v-18.875c0-0.531-0.406-0.969-0.906-0.969h-6.625c-0.531 0-0.969 0.438-0.969 0.969zM12.281 6.563v18.875c0 0.531 0.438 0.969 0.938 0.969h6.625c0.531 0 0.969-0.438 0.969-0.969v-18.875c0-0.531-0.438-0.969-0.969-0.969h-6.625c-0.5 0-0.938 0.438-0.938 0.969z"></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+const ArrowRight = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="w-4 h-4" {...props}>
+      <g strokeWidth="0"></g>
+      <g strokeLinecap="round" strokeLinejoin="round"></g>
+      <g>
+        <path
+          stroke="#ffffff"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          d="M2 10h16m0 0l-7-7m7 7l-7 7"
+        ></path>
+      </g>
+    </svg>
+  );
+};
+
+const VolumeIcon = (props: SVGProps<SVGSVGElement>) => {
+  return (
+    <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#ffffff" {...props}>
+      <g strokeWidth="0"></g>
+      <g strokeLinecap="round" strokeLinejoin="round"></g>
+      <g>
+        <g id="ICONS">
+          <path
+            fill="#ffffff"
+            d="M545.8 294.7L363.7 431.5c-1.2.9-2 2.1-2.7 3.3H256v154.5h105c.7 1.2 1.6 2.4 2.7 3.3l182.1 136.7c7.2 5.4 17.5.3 17.5-8.7V303.5c0-9-10.3-14.2-17.5-8.8zM668 691.7c-8.8 0-17.4-4.5-22.2-12.7-7.1-12.2-3-27.9 9.2-35 2.4-1.4 61.7-38.4 61.7-132 0-95-61.1-131.6-61.7-132-12.2-7.1-16.3-22.8-9.2-35 7.1-12.2 22.8-16.3 35-9.2 3.7 2 87.2 52.2 87.2 176.2s-83.5 174.2-87.1 176.2c-4.1 2.4-8.5 3.5-12.9 3.5z"
+          ></path>{" "}
+          <path
+            fill="#ffffff"
+            d="M613.2 621.2c-8.8 0-17.4-4.5-22.1-12.7-7.1-12.2-3-27.9 9.2-35 .7-.4 24.6-16 24.6-55.1s-23.9-54.7-25-55.4c-11.8-7.4-15.7-23.1-8.4-35 7.2-12 22.5-16 34.6-9 2 1.2 50 29.9 50 99.4s-48 98.2-50 99.4c-4.1 2.3-8.5 3.4-12.9 3.4z"
+          ></path>{" "}
+        </g>{" "}
+      </g>
+    </svg>
+  );
+};
